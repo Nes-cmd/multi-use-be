c0ef513caaf4bc45ebcc5b41a721085a1f5e75ad
Nesru <nesrusadik6@gmail.com>
2024-11-19 18:56:46 +0300

    fds

--- app/Http/Controllers/TefelagiController.php
+++ app/Http/Controllers/TefelagiController.php
@@ -7,12 +7,13 @@ use Illuminate\Support\Facades\Log;
 
 class TefelagiController extends Controller
 {
-    public function tobeCalled()  {
+    public function tobeCalled()
+    {
         Log::info('was hited');
-        return response([
+        return response()->json([
             'status' => 1,
             'phone' => '+251940678725',
-            'message' => 'Your prospect was found around piassa, for more information.'
-        ]);
+            'message' => 'piassa'
+        ])->setEncodingOptions(JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
     }
 }
