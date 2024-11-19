4be10f47397581f68c357d053a08d851748d59de
Nesru <nesrusadik6@gmail.com>
2024-11-19 17:04:58 +0300

    log

--- app/Http/Controllers/TefelagiController.php
+++ app/Http/Controllers/TefelagiController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class TefelagiController extends Controller
 {
     public function tobeCalled()  {
+        Log::info('was hited');
         return response([
             'status' => 1,
             'phone' => '+251940678725',
